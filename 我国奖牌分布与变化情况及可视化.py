8e5249dd4a2c3351ca0dbd6a5196c33fbd48b98d
Yehu-cp <61683267+Smartloe@users.noreply.github.com>
2021-11-23 16:47:22 +0800

    update

--- 我国奖牌分布与变化情况及可视化.py
+++ 我国奖牌分布与变化情况及可视化.py
@@ -264,7 +264,14 @@ def chart_three(data):
 	bronzes_num =sum(data[2]['名次'])/3
 	data = [('金牌',golds_num), ('银牌',silvers_num), ('铜牌',bronzes_num)]
 	pie = (
-		Pie(init_opts=opts.InitOpts(width="100%",height="750px",page_title="中国队金、银、铜牌占比"))
+		Pie(init_opts=opts.InitOpts(
+									width="100%",
+									height="750px",
+									page_title="中国队金、银、铜牌占比",
+									theme=ThemeType.INFOGRAPHIC,
+									bg_color={"type": "pattern", "image": JsCode("img"), "repeat": "no-repeat"}
+								)
+			)
 		.add(
 			"",
 			data,
@@ -274,8 +281,13 @@ def chart_three(data):
 			label_opts=opts.LabelOpts(is_show=False),
 		)
 		.set_series_opts(label_opts=opts.LabelOpts(formatter="{b}: {c}"))
-		.render("中国队金、银、铜牌占比.html")
 	)
+	pie.add_js_funcs(
+		"""
+		var img = new Image(); img.src = '运动图标/金牌榜.png';
+		"""
+	)
+	pie.render("中国队金、银、铜牌占比.html")
 	return pie
 
 
